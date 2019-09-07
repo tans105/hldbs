99bdc22319c8c554be26419e72729809592d54ae
Tanmay <tanmayawasthi105@gmail.com>
2019-09-07 14:09:47 +0530

    init commit

---
+++ src/main/java/com/example/hldbs/HldbsApplication.java
@@ -0,0 +1,12 @@
+package com.example.hldbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HldbsApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(HldbsApplication.class, args);
+    }
+}
